06ff765e1d08717057ed758f6fcba738457dd6b9
alamdilshad87 <dilshadand@gmail.com>
2026-01-17 01:19:48 +0530

    feat(login): Login Card with proper alignment and responsiveness

--- fundoonotes-ui/src/app/auth/login/login.ts
+++ fundoonotes-ui/src/app/auth/login/login.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrls: ['./login.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+}
